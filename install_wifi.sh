f08b5080276fc389b8df9dbb575e8df297aaf913
Augustin900 <gaugu90@gmail.com>
2025-06-13 17:09:48 +0300

    Add script

---
+++ install_wifi.sh
@@ -0,0 +1,11 @@
+# Install git if not availabile
+sudo apt install git -y
+
+# Clone repo
+git clone https://github.com/morrownr/8821au-20210708
+
+# Go to repo folder
+cd 8821au-20210708
+
+# Install driver
+sudo ./install-driver.sh
